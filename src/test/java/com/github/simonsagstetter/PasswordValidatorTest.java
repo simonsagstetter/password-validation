b92f3246e0ce86fa1813dad07cdf1d23d31671c9
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-10-14 15:08:23 +0200

    Updated PasswordValidatorTest class and bundled hasMinLength test methods into a nested test group for readability, added display names

--- src/test/java/com/github/simonsagstetter/PasswordValidatorTest.java
+++ src/test/java/com/github/simonsagstetter/PasswordValidatorTest.java
@@ -1,63 +1,79 @@
 package com.github.simonsagstetter;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordValidatorTest {
 
-    @Test
-    void hasMinLength_ShouldReturnFalse_WhenCalledWith7Chars(){
-        String password = "BadPass";
 
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
+    @Nested
+    @DisplayName("all tests for method hasMinLength in PasswordValidator class")
+    class hasMinLengthTest {
 
-        assertFalse(hasMinLength);
+        @Test
+        @DisplayName("should return false when called with a string that has only 7 characters")
+        void hasMinLength_ShouldReturnFalse_WhenCalledWith7Chars(){
+            String password = "BadPass";
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertFalse(hasMinLength);
+        }
+
+        @Test
+        @DisplayName("should return true when called with a string that has 8 characters")
+        void hasMinLength_ShouldReturnTrue_WhenCalledWith8Chars(){
+            String password = "Passwords";
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertTrue(hasMinLength);
+        }
+
+        @Test
+        @DisplayName("should return true when called with a string that has 9 characters")
+        void hasMinLength_ShouldReturnTrue_WhenCalledWith9Chars(){
+            String password = "Passwords";
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertTrue(hasMinLength);
+        }
+
+        @Test
+        @DisplayName("should return false when called with an empty string")
+        void hasMinLength_ShouldReturnFalse_WhenCalledWithEmptyString(){
+            String password = "          ";
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertFalse(hasMinLength);
+        }
+
+        @Test
+        @DisplayName("should return false when called with a string that has white spaces")
+        void hasMinLength_ShouldReturnFalse_WhenCalledWithWhiteSpaces(){
+            String password = "    test      ";
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertFalse(hasMinLength);
+        }
+
+        @Test
+        @DisplayName("should return false when called with a string that is null")
+        void hasMinLength_ShouldReturnFalse_WhenCalledWithNull(){
+            String password = null;
+
+            boolean hasMinLength = PasswordValidator.hasMinLength(password);
+
+            assertFalse(hasMinLength);
+        }
+
     }
 
-    @Test
-    void hasMinLength_ShouldReturnTrue_WhenCalledWith8Chars(){
-        String password = "Passwords";
-
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
-
-        assertTrue(hasMinLength);
-    }
-
-    @Test
-    void hasMinLength_ShouldReturnTrue_WhenCalledWith9Chars(){
-        String password = "Passwords";
-
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
-
-        assertTrue(hasMinLength);
-    }
-
-    @Test
-    void hasMinLength_ShouldReturnFalse_WhenCalledWithEmptyString(){
-        String password = "          ";
-
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
-
-        assertFalse(hasMinLength);
-    }
-
-    @Test
-    void hasMinLength_ShouldReturnFalse_WhenCalledWithWhiteSpaces(){
-        String password = "    test      ";
-
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
-
-        assertFalse(hasMinLength);
-    }
-
-    @Test
-    void hasMinLength_ShouldReturnFalse_WhenCalledWithNull(){
-        String password = null;
-
-        boolean hasMinLength = PasswordValidator.hasMinLength(password);
-
-        assertFalse(hasMinLength);
-    }
 
 }
