f8e5a65c4f9d0a456e109d6acf3adfa90684f31d
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-10-14 15:37:51 +0200

    Added containsDigit method to PasswordValidator class, Added related test methods to PasswordValidator class

--- src/main/java/com/github/simonsagstetter/PasswordValidator.java
+++ src/main/java/com/github/simonsagstetter/PasswordValidator.java
@@ -3,9 +3,19 @@ package com.github.simonsagstetter;
 public final class PasswordValidator {
 
     private static final int minLengthDefault = 8;
+    private static final String[] digits = {"1", "2", "3" , "4", "5", "6", "7", "8", "9", "0"};
 
     public static boolean hasMinLength(String password){
         return password != null && password.trim().length() >= minLengthDefault;
+    }
+
+    public static boolean containsDigit(String password){
+        for(String s : password.split("")){
+            for(String d: digits){
+                if(s.equals(d))return true;
+            }
+        }
+        return false;
     }
 
 }
