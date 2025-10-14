273efcdeb85ef8ba0fd57e14b2d42207317339d8
Simon Sagstetter <simon.sagstetter95@gmail.com>
2025-10-14 16:35:46 +0200

    Added containsUpperAndLower method to PasswordValidator class, Added related test methods to PasswordValidator class

--- src/main/java/com/github/simonsagstetter/PasswordValidator.java
+++ src/main/java/com/github/simonsagstetter/PasswordValidator.java
@@ -15,4 +15,17 @@ public final class PasswordValidator {
         return false;
     }
 
+    public static boolean containsUpperAndLower(String password){
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+
+        for(char c: password.toCharArray()){
+            if(!hasLowerCase && c >= 'a' && c <= 'z')hasLowerCase = true;
+            else if(!hasUpperCase && c >= 'A' && c <= 'Z')hasUpperCase = true;
+            if(hasLowerCase && hasUpperCase)return true;
+        }
+
+        return false;
+    }
+
 }
